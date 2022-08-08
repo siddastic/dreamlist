daab8b1d2cf91b34f405c88f99e6434adede2120
siddastic <siddhantgaurav2003@gmail.com>
2022-08-08 22:39:00 +0530

    implement google auth

--- lib/screens/auth/login.dart
+++ lib/screens/auth/login.dart
@@ -1,12 +1,38 @@
+import 'package:dreamlist/screens/home/home.dart';
 import 'package:dreamlist/widgets/fade_animation.dart';
 import 'package:dreamlist/widgets/v_space.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_signin_button/button_list.dart';
 import 'package:flutter_signin_button/button_view.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   static const routeName = '/login';
   const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  Future<UserCredential> signInWithGoogle() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser?.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    // Once signed in, return the UserCredential
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -50,7 +76,16 @@ class LoginScreen extends StatelessWidget {
             child: SignInButton(
               Buttons.Google,
               text: "Sign up with Google",
-              onPressed: () {},
+              onPressed: () async {
+                try {
+                  final UserCredential userCredential =
+                      await signInWithGoogle();
+                  Navigator.of(context)
+                      .pushReplacementNamed(HomeScreen.routeName);
+                } catch (e) {
+                  print(e);
+                }
+              },
             ),
           ),
           const VSpace(h: 20),
